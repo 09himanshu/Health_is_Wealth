e34df5b852ea4892dad1afe77e6fc3765c8a4c32
Himanshu Sah <09himanshusah@gmail.com>
2022-05-28 23:26:30 +0530

    Work on 28-5-2022

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -1,19 +1,95 @@
 // This file have the entire crud operation logic
 
 const db = require('../models');
-const Appomit = db.appoint;
+const Apponit = db.appoint;
+const User = db.user;
+const Doctor = db.doctor;
 
 // Handler for create appointment
 exports.create = (req, res) => {
     const obj = {
-        userId: req.body.useerId,
+        userId: req.body.userId,
         doctorId: req.body.doctorId,
         date: req.body.date,
         time: req.body.time,
     }
-    Appomit.create(obj).then(appoint => {
+    Apponit.create(obj).then(appoint => {
         res.status(201).send(appoint)
     }).catch(err => {
         res.status(500).send({message: `Error occur at appoint creation ${err}`});
     });
+}
+
+// Handler for get appointment
+exports.findAll = (req, res) => {
+    let promise;
+    let date = req.query.date;
+    if(date) {
+        promise = Apponit.findAll({
+            where: {date}
+        })
+    }else {
+        promise = Apponit.findAll();
+    }
+    promise.then(all => {
+        res.status(200).send(all);
+    }).catch(err => {
+        res.status(500).send({message: `Error occur at get all appointment ${err}`});
+    })
+}
+
+// Handler for get appointment by id
+exports.findOne = (req, res) => {
+    let id = req.params.id;
+    let paitentName;
+    let doctorName;
+    Apponit.findByPk(id).then(data => {
+        if(!data) {
+            return res.status(404).send({message: `No appointment fixed yet`});
+        }
+        User.findByPk(data.userId).then(user => {
+            paitentName = user.firstName+' '+user.lastName;
+
+            Doctor.findByPk(data.doctorId).then(doctor => {
+                doctorName = doctor.firstName+' '+doctor.lastName;
+                res.status(200).send({
+                    paitentName: paitentName,
+                    doctorName: doctorName,
+                })
+            });
+        });
+    }).catch(err => {
+        res.status(500).send({message: `Error occur at fetch appointment by id ${err}`});
+    })
+}
+
+// Handler for update
+exports.update = (req, res) => {
+    const id = req.params.id;
+    const obj = {
+        date: req.body.date,
+        time: req.body.time,
+    }
+    Apponit.update(obj, {
+        where: {id},
+        returning: true,
+    }).then(() => {
+        Apponit.findByPk(id).then(update => {
+            res.status(202).send(update);
+        })
+    }).catch(err => {
+        res.status(500).send({ message: `Error occur at update ${err}`});
+    })
+}
+
+// Handler for delete
+exports.delete = (req, res) => {
+    let id = req.params.id;
+    Apponit.destroy({
+        where: {id},
+    }).then(() => {
+        res.status(204).send({message: `Appointment deleted successfully`});
+    }).catch(err => {
+        res.status(500).send({message: `Error occur at delete hospital ${err}`});
+    })
 }
--- routes/appoint.routes.js
+++ routes/appoint.routes.js
@@ -5,5 +5,17 @@ const controller = require('../controllers/appointment.controller');
 module.exports = (app) => {
 
     // Route for appointment creation
-    app.post('/health/api/v1/hospitals', controller.create);
+    app.post('/health/api/v1/appoint', controller.create);
+
+    // Route for get all appointment
+    app.get('/health/api/v1/appoint', controller.findAll);
+
+    // Route for get  appointment based on id
+    app.get('/health/api/v1/appoint/:id', controller.findOne);
+
+    // Route for update appointment
+    app.put('/health/api/v1/appoint/:id', controller.update);
+
+    // Route for delete appointment
+    app.delete('/health/api/v1/appoint/:id', controller.delete);
 }
